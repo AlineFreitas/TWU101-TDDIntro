97a1a28aabe17189768af8f001096704281cd09e
AlineFreitas <aline.do.freitas@gmail.com>
2019-03-29 15:33:00 -0300

    Finished Factorial Class

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,26 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
-public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+public class Factorial
+{
+    public Integer compute(int i)
+    {
+        int factor;
+        int resultado = 1;
+
+        if (i < 0){
+            throw new IllegalArgumentException();
+        }
+
+        else {
+            if (i>1)
+            {
+                for(factor=1; factor<=i; factor++)
+                {
+                    resultado = resultado * factor;
+                }
+            }
+
+            return resultado;
+        }
     }
 }
